f5dd612ff7131d7ac6c41af37128e502f23dfa36
Phellpzcg <157931848+Phellpzcg@users.noreply.github.com>
2026-05-02 02:10:22 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1041,7 +1041,7 @@ var baseDeConhecimento = [
     url: "alteracoes-e-crescimento/baixa-e-encerramento-de-empresa.html",
     icone: '<path d="M3 6h18"/><path d="M19 6v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6m3 0V4a2 2 0 012-2h4a2 2 0 012 2v2"/><line x1="10" y1="11" x2="10" y2="17"/><line x1="14" y1="11" x2="14" y2="17"/>',
     palavras_chave: ["baixar cnpj", "fechar", "encerramento", "fim empresa", "distrato", "cancelar cnpj"]
-  }
+  },
 
   {
     id: 101, 
